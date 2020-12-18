c440d4ba023efb0e2aeb78ed463fe144a00f930b
Yury Vinogradov <yuvinogradov@gmail.com>
2020-12-18 21:10:04 +0200

    added built project files

---
+++ precache-manifest.089684b80f7e8c2e6edf43da49a008fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef506d099a62eddd742856d149cf9754",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48d7e243a9955a400a9d",
+    "url": "/static/css/main.d780d2b8.chunk.css"
+  },
+  {
+    "revision": "de33c7581e3adc69be01",
+    "url": "/static/js/2.4f7c5358.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4f7c5358.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48d7e243a9955a400a9d",
+    "url": "/static/js/main.8bbd3253.chunk.js"
+  },
+  {
+    "revision": "5a60fc9df39256935c27",
+    "url": "/static/js/runtime-main.1e2b0c03.js"
+  }
+]);
